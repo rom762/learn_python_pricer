aadfbceee315d20703f20e9c21dc0b492f621ae6
nickr <nickro@ya.ru>
2021-03-05 13:53:03 +0300

    made templates

--- server.py
+++ server.py
@@ -3,11 +3,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+menu = {
+    'Home': '/',
+    'News': '/news',
+    'Weather': '/weather',
+    'Register': '/register',
+}
+
 
 @app.route('/')
 def index():
     title = 'Pricer web-service'
-    return render_template('index.html', page_title=title)
+    return render_template('index.html', page_title=title, menu=menu)
 
 
 if __name__ == "__main__":
